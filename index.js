98ebcb781112eced3335445c1f6563782cf0d793
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-/**
- * jstransformer-css <https://github.com/jstransformers/jstransformer-css>
- *
- * Copyright (c) 2015 Charlike Mike Reagent, contributors.
- * Released under the MIT license.
- */
+'use strict'
 
-'use strict';
+var css = require('css')
 
-var css = require('css');
-
-exports.name = 'css';
-exports.outputFormat = 'json';
+exports.name = 'css'
+exports.outputFormat = 'json'
 
 exports.render = function (str, opts) {
-  return JSON.stringify(css.parse(str, opts));
-};
+  return JSON.stringify(css.parse(str, opts))
+}
